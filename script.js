0e178220ef6e0a7fd7e0050d483e815ae13631ac
jayankoliya <111362462+jayankoliya@users.noreply.github.com>
2023-10-10 09:30:59 +0530

    Crypto_Currency_price

---
+++ script.js
@@ -0,0 +1,16 @@
+let bitcoin = document.getElementById("bitcoin");
+let ethereum = document.getElementById("ethereum");
+let dogecoin = document.getElementById("dogecoin");
+
+var settings = {
+    "async": true,
+    "scrossDomain": true,
+    "url":"https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2CDogecoin&vs_currencies=usd",
+    "method":"GET",
+    "headers":{}
+}
+$.ajax(settings).done(function (response){
+    bitcoin.innerHTML = response.bitcoin.usd;
+    ethereum.innerHTML = response.ethereum.usd;
+    dogecoin.innerHTML = response.dogecoin.usd;
+})
